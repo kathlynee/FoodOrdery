f83f884499aaa10e29ab775d646f883a36dbea90
kathlynee <ksarahfania@student.ciputra.ac.id>
2023-05-30 22:13:51 +0700

    Initial Commit

---
+++ FoodOrdery/FoodOrderyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrderyApp.swift
+//  FoodOrdery
+//
+//  Created by MacBook Pro on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodOrderyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
